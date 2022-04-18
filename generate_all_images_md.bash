40859696dd40d929110646d79a15d41124fa3f51
Daisuke Sato <daisuke.sato@rt-net.jp>
2022-04-18 16:20:51 +0900

    Add copyright

--- generate_all_images_md.bash
+++ generate_all_images_md.bash
@@ -31,3 +31,5 @@ echo -e ${OUTPUT} > all-images.md
 
 # CC-BYを追記
 echo '<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />The files in this page are licensed under the <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.' >> all-images.md
+echo "" >> all-images.md
+echo "(C) 2016 RT Coporation \<support@rt-net.jp\>" >> all-images.md
